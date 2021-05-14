1f4caf1497068f244413cfc75516b44d72ee0067
Oliver Heinimann <oliver.heinimann@unibas.ch>
2021-05-14 12:37:53 +0200

    Adjusted structure in main notebook. Adjustment of hyper parameter to function structure to allow more efficient tuning.

--- dqn_agent_backup.py
+++ dqn_agent_backup.py
@@ -8,21 +8,19 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-"""
 BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate 
 UPDATE_EVERY = 4        # how often to update the network
-"""
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Agent():
     """Interacts with and learns from the environment."""
 
-    def __init__(self, state_size, action_size, seed, buffer_size=int(1e5), batch_size=64, gamma=0.99, tau=1e-3, lr=5e-4, update_every=4):
+    def __init__(self, state_size, action_size, seed):
         """Initialize an Agent object.
         
         Params
@@ -30,33 +28,18 @@ class Agent():
             state_size (int): dimension of each state
             action_size (int): dimension of each action
             seed (int): random seed
-            buffer_size (int): replay buffer size
-            batch_size (int): minibatch size
-            gamma (float): discount factor
-            tau (float): for stof update of target parameters
-            lr (float): learning rate
-            update_every (int): how often to update the network
         """
-        # Env parameters
         self.state_size = state_size
         self.action_size = action_size
         self.seed = random.seed(seed)
-        # DL parameters
-        self.buffer_size = buffer_size
-        self.batch_size = batch_size
-        self.gamma = gamma
-        self.tau = tau
-        self.lr = lr
-        self.update_every = update_every
-
 
         # Q-Network
         self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
         self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
-        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=self.lr)
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
 
         # Replay memory
-        self.memory = ReplayBuffer(action_size, self.buffer_size, self.batch_size, seed)
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
         # Initialize time step (for updating every UPDATE_EVERY steps)
         self.t_step = 0
     
@@ -65,12 +48,12 @@ class Agent():
         self.memory.add(state, action, reward, next_state, done)
         
         # Learn every UPDATE_EVERY time steps.
-        self.t_step = (self.t_step + 1) % self.update_every
+        self.t_step = (self.t_step + 1) % UPDATE_EVERY
         if self.t_step == 0:
             # If enough samples are available in memory, get random subset and learn
-            if len(self.memory) > self.batch_size:
+            if len(self.memory) > BATCH_SIZE:
                 experiences = self.memory.sample()
-                self.learn(experiences, self.gamma)
+                self.learn(experiences, GAMMA)
 
     def act(self, state, eps=0.):
         """Returns actions for given state as per current policy.
@@ -118,7 +101,7 @@ class Agent():
         self.optimizer.step()
 
         # ------------------- update target network ------------------- #
-        self.soft_update(self.qnetwork_local, self.qnetwork_target, self.tau)                     
+        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)                     
 
     def soft_update(self, local_model, target_model, tau):
         """Soft update model parameters.
